871153a27fc50725fbcd46648910bd063e92c591
juanguz1999 <juanguz1999@outlook.com>
2022-05-11 02:21:37 -0500

    Avanzado 2

--- src/Formulario/interfaz.java
+++ src/Formulario/interfaz.java
@@ -1,5 +1,6 @@
 package Formulario;
 import Controlador.*;
+import java.text.DecimalFormat;
 
 public class interfaz extends javax.swing.JFrame {
 
@@ -35,17 +36,16 @@ public class interfaz extends javax.swing.JFrame {
         lbNivelDeConfianza = new javax.swing.JLabel();
         lbAlfa = new javax.swing.JLabel();
         lbZ = new javax.swing.JLabel();
-        jLabel14 = new javax.swing.JLabel();
-        jLabel15 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         jLabel13 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
+        txts = new javax.swing.JTextArea();
         jButton2 = new javax.swing.JButton();
         jLabel16 = new javax.swing.JLabel();
-        jLabel17 = new javax.swing.JLabel();
-        jLabel18 = new javax.swing.JLabel();
+        intAGrafico = new javax.swing.JLabel();
+        intBgrafico = new javax.swing.JLabel();
         jLabel19 = new javax.swing.JLabel();
+        interpretacion = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -96,10 +96,6 @@ public class interfaz extends javax.swing.JFrame {
 
         cbNivelConfianza.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccionar", "50%", "60%", "70%", "80%", "90%", "95%", "98%", "99%" }));
 
-        jLabel14.setText("Intervalo 1");
-
-        jLabel15.setText("intervalo 2");
-
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -144,9 +140,7 @@ public class interfaz extends javax.swing.JFrame {
                                         .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addComponent(jLabel10, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addComponent(jLabel11, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(jLabel12, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                    .addComponent(jLabel14)
-                                    .addComponent(jLabel15))
+                                        .addComponent(jLabel12, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE)))
                                 .addGap(52, 52, 52)
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                                     .addComponent(lbPromedio)
@@ -209,11 +203,7 @@ public class interfaz extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel12)
                     .addComponent(lbZ))
-                .addGap(18, 18, 18)
-                .addComponent(jLabel14)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel15)
-                .addContainerGap(13, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         jPanel2.setBackground(new java.awt.Color(255, 255, 255));
@@ -221,9 +211,9 @@ public class interfaz extends javax.swing.JFrame {
         jLabel13.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLabel13.setText("Solución");
 
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jScrollPane1.setViewportView(jTextArea1);
+        txts.setColumns(20);
+        txts.setRows(5);
+        jScrollPane1.setViewportView(txts);
 
         jButton2.setText("Reiniciar");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
@@ -234,27 +224,29 @@ public class interfaz extends javax.swing.JFrame {
 
         jLabel16.setText("Grafico");
 
-        jLabel17.setText("intervalo 1");
+        intAGrafico.setText("intervalo 1");
 
-        jLabel18.setText("intervalo 2");
+        intBgrafico.setText("intervalo 2");
 
         jLabel19.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLabel19.setText("Procedimiento");
+
+        interpretacion.setText("jLabel17");
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addGap(189, 189, 189)
+                .addComponent(intAGrafico)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(intBgrafico)
+                .addGap(204, 204, 204))
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGap(332, 332, 332)
                 .addComponent(jLabel16)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(163, 163, 163)
-                .addComponent(jLabel17)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel18)
-                .addGap(201, 201, 201))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
                 .addContainerGap(39, Short.MAX_VALUE)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -268,26 +260,31 @@ public class interfaz extends javax.swing.JFrame {
                         .addGap(327, 327, 327))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 678, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(22, 22, 22))))
+                        .addGap(22, 22, 22))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                        .addComponent(interpretacion)
+                        .addGap(340, 340, 340))))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(40, 40, 40)
-                .addComponent(jLabel13)
-                .addGap(42, 42, 42)
-                .addComponent(jLabel16)
-                .addGap(83, 83, 83)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel17)
-                    .addComponent(jLabel18))
                 .addGap(18, 18, 18)
+                .addComponent(jLabel13)
+                .addGap(29, 29, 29)
+                .addComponent(jLabel16)
+                .addGap(84, 84, 84)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(intBgrafico)
+                    .addComponent(intAGrafico))
+                .addGap(52, 52, 52)
                 .addComponent(jLabel19)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 228, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 194, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(interpretacion)
+                .addGap(24, 24, 24)
                 .addComponent(jButton2)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(21, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -364,11 +361,21 @@ public class interfaz extends javax.swing.JFrame {
         lbZ.setText(MZ);
         
         String Ma =  String.valueOf(c.intervaloA());
-        jLabel14.setText(Ma);
+        intAGrafico.setText(Ma);
         
         String Mb =  String.valueOf(c.intervaloB());
-        jLabel15.setText(Mb);
+        intBgrafico.setText(Mb);
         
+        
+        //Procedimiento
+        txts.setText("");
+        DecimalFormat df = new DecimalFormat("###.##");
+        double inta = c.intervaloA();
+        txts.append(df.format(inta)+"<");
+        
+        
+        //interpretacion
+        interpretacion.setText("Con un " + nc +"%");
     }//GEN-LAST:event_btnCalcularActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
@@ -376,6 +383,16 @@ public class interfaz extends javax.swing.JFrame {
         txtMuestra.setText("");
         txtProm.setText("");
         txtDesviacionEstandar.setText("");
+        interpretacion.setText("");
+        intAGrafico.setText("");
+        intBgrafico.setText("");
+        lbAlfa.setText("");
+        lbDesviacionEstandar.setText("");
+        lbMuestra.setText("");
+        lbNivelDeConfianza.setText("");
+        lbPromedio.setText("");
+        lbZ.setText("");
+        txts.setText("");
         
     }//GEN-LAST:event_jButton2ActionPerformed
 
@@ -417,17 +434,16 @@ public class interfaz extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnCalcular;
     private javax.swing.JComboBox<String> cbNivelConfianza;
+    private javax.swing.JLabel intAGrafico;
+    private javax.swing.JLabel intBgrafico;
+    private javax.swing.JLabel interpretacion;
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel12;
     private javax.swing.JLabel jLabel13;
-    private javax.swing.JLabel jLabel14;
-    private javax.swing.JLabel jLabel15;
     private javax.swing.JLabel jLabel16;
-    private javax.swing.JLabel jLabel17;
-    private javax.swing.JLabel jLabel18;
     private javax.swing.JLabel jLabel19;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -440,7 +456,6 @@ public class interfaz extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea jTextArea1;
     private javax.swing.JLabel lbAlfa;
     private javax.swing.JLabel lbDesviacionEstandar;
     private javax.swing.JLabel lbMuestra;
@@ -450,5 +465,6 @@ public class interfaz extends javax.swing.JFrame {
     private javax.swing.JTextField txtDesviacionEstandar;
     private javax.swing.JTextField txtMuestra;
     private javax.swing.JTextField txtProm;
+    private javax.swing.JTextArea txts;
     // End of variables declaration//GEN-END:variables
 }
